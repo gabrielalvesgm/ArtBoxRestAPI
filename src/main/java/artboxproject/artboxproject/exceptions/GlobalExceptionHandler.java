88e10e24904c7c1b320442e6644b715f88dfc2af
GabrielAlvesGM <lgabrielalvesgm@gmail.com>
2025-02-19 18:39:06 -0300

    configured GlobalExceptionHandler

--- src/main/java/artboxproject/artboxproject/exceptions/GlobalExceptionHandler.java
+++ src/main/java/artboxproject/artboxproject/exceptions/GlobalExceptionHandler.java
@@ -1,4 +1,36 @@
 package artboxproject.artboxproject.exceptions;
 
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+//Esta é a classe de captura de exceções globais.
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+
+    //Captura quando um recurso não for encontrado (ex: ID inválido)
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleNotFoundException(EntityNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+
+
+    //Captura erros de argumento inválido (ex: número negativo)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException (IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+
+
+    //Captura erros genéricos do sistema
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro interno do sistema. Contate o suporte.");
+    }
 }
